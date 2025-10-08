6f4491e299789981f7b0386cddc66163f69954a0
TaengSuraphong <benkbom99@gmail.com>
2025-10-08 09:53:32 +0700

    Edit Final

--- src/components/LoadingPages.jsx
+++ src/components/LoadingPages.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
 import "../styles/LoadingPages.scss";
+import logo from "../assets/Logo_CCC.png";
 
 class LoadingPages extends React.Component {
-    state = {
-        number: 0,
-    };
-    componentDidMount() {
-        this.interval = setInterval(()=> {
-            this.setState((prevState) => ({number: prevState.number + 1}));
-        }, 15);
-    }
-    componentWillUnmount(){
-        clearInterval(this.interval);
-    }
+  state = { number: 0 };
 
-    render() {
-        return (
-            <div className='flex-container'>
-                <h1 className="box-progress" data-text='CCC THAILAND'>
-                    CCC THAILAND
-                </h1>
-                <span className='box-text'>{this.state.number}%</span>
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(prev => {
+        const next = Math.min(prev.number + 1, 100); // ไม่เกิน 100
+        if (next === 100) clearInterval(this.interval);
+        return { number: next };
+      });
+    }, 15);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { number } = this.state;
+
+    return (
+      <div className="flex-container">
+        <div className="box-progress" data-text="" style={{ '--p': `${number}%` }}>
+            <img src={logo} alt="CCC Logo" />
+            <span className="box-text">{number}%</span>
+        </div>
+    </div>
+    );
+  }
 }
 
-export default LoadingPages
+export default LoadingPages;
